6f155abf09cb561d84b2114a26deeca02effef55
shiva <hunkr11@gmail.com>
2015-03-17 12:19:10 +0530

    Campus_templates

--- Campus/src/main/java/com/erp/campus/controller/AcademicController.java
+++ Campus/src/main/java/com/erp/campus/controller/AcademicController.java
@@ -3,14 +3,23 @@ package com.erp.campus.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-//@RequestMapping(value = "module/academic")
+@RequestMapping(value = "/Campus/module/academic")
 public class AcademicController {
 	
-	@RequestMapping(value = "module/academic",method = RequestMethod.GET)
+	//@RequestMapping(value = "/module/academic",method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public String academic(){
 		System.out.println("ACADEMICS MODULE");
-		return "academic/home";
+		return "academic/homeAcademic";
+	}
+	
+	@RequestMapping(value="/admission",method=RequestMethod.GET)
+	public ModelAndView admission(){
+		System.out.println(" \n AcademicController ADMISSION ");
+		ModelAndView mav = new ModelAndView("academic/admission");		
+		return mav;
 	}
 }
